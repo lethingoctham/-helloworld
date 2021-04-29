e3a5b0809da661ed7a684422ec4441e3814b2191
lethingoctham <72124921+lethingoctham@users.noreply.github.com>
2021-04-29 10:28:41 +0700

    Add files via upload

--- Su_dung_truc_quan.py
+++ Su_dung_truc_quan.py
@@ -50,15 +50,14 @@ class visualization:
         return percentile
     
     def hist(self):
-        plt.hist(self.data, density=2, color = 'pink')
+        plt.hist(self.data,20, density=2, color = 'skyblue', edgecolor = 'green')
         mu = 0
         sigma = math.sqrt(np.std(self.data))
         x = np.linspace(mu - 3 * sigma, mu + 3 * sigma, 100)
         plt.plot(x, stats.norm.pdf(x, mu, sigma), color = 'green')
-        return plt.show()
     
     def qq_plot(self):
-        qq = sm.qqplot(self.data, line = '45')
+        qq = sm.qqplot(self.data, line = '45', color = 'y')
         plt.show(qq)
         return qq
     
@@ -73,5 +72,5 @@ print("quantile 0.25 = ", vi.quantile(0.25), "\n",
       "quantile 0.5 = ", vi.quantile(0.5),"\n" ,
       "quantile 0.75 = ", vi.quantile(0.75),"\n")
 
-print("hist = ", vi.hist())
-print( vi.qq_plot())
+vi.hist()
+vi.qq_plot()
